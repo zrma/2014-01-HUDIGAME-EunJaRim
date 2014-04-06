24d133f295ea3bf3391097b9032df0ec6d7ef644
#1 DC Shin <dcflight@naver.com>
2014-04-06 21:47:45 +0900

    dxd dll version #1

---
+++ YaMang/yaMangDxDll/yaMangDxDll/yaMangDxDll.cpp
@@ -0,0 +1,225 @@
+// yaMangDxDll.cpp : DLL 응용 프로그램을 위해 내보낸 함수를 정의합니다.
+//
+
+#include "stdafx.h"
+#include "yaMangDxDll.h"
+
+
+//////////////////////////////////////////////////////////////////////////
+//Global val 선언
+//////////////////////////////////////////////////////////////////////////
+//부모 Process 공용 자원
+LPDIRECT3D9 D3D = nullptr;
+LPDIRECT3DDEVICE9 D3dDevice = nullptr;
+
+//yaMangDxDll 내부 사용에서 각 객체가 들고 있어야 된다 판단되어 삭제
+//DWORD g_NumMaterials = 0L;
+
+//////////////////////////////////////////////////////////////////////////
+//input args: 윈도우 핸들
+//향후 공용으로 사용할 D3D, D3DDevice에 대해 초기화 진행
+//프로그램에서 1회만 실행 후 Process 종료까지 사용
+//참고url: http://www.delmadang.com/community/bbs_view.asp?bbsNo=17&indx=426040 
+//////////////////////////////////////////////////////////////////////////
+YAMANGDXDLL_API HRESULT InitD3D( HWND hWnd )
+{
+	if ( nullptr == ( D3D = Direct3DCreate9( D3D_SDK_VERSION ) ) )
+	{
+		// 오류
+		MessageBox( NULL, L"Could not Create D3D", L"Meshes.exe", MB_OK );
+		return E_FAIL;
+	}
+
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory( &d3dpp, sizeof( d3dpp ) );
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	d3dpp.EnableAutoDepthStencil = TRUE;
+	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+
+	if ( FAILED (D3D->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &D3dDevice ) ) )
+	{
+		MessageBox( NULL, L"Could not CreateDevice", L"Meshes.exe", MB_OK );
+		return E_FAIL;
+	}
+
+	D3dDevice->SetRenderState( D3DRS_CULLMODE, D3DCULL_NONE );
+	D3dDevice->SetRenderState( D3DRS_ZENABLE, TRUE );
+
+	return S_OK;
+}
+
+YAMANGDXDLL_API HRESULT InitGeometry( HWND hWnd, LPCTSTR fileName, MESHOBJECT* inputVal )
+{
+	LPD3DXBUFFER D3dxMtrialBuffer;
+
+	//x file import
+	if (FAILED(D3DXLoadMeshFromX(fileName, D3DXMESH_SYSTEMMEM, D3dDevice, NULL, &D3dxMtrialBuffer, NULL, &(inputVal->NumMaterials), &inputVal->importedMesh)))
+	{
+		MessageBox( NULL, L"Could not find x file", L"Meshes.exe", MB_OK );
+		return E_FAIL;
+	}
+
+
+	//import한 mesh에 normal 값이 없는 경우 normal 계산
+	if ( !( ( inputVal->importedMesh )->GetFVF()&D3DFVF_NORMAL ) )
+	{
+		ID3DXMesh* pTempMesh = 0;
+		( inputVal->importedMesh )->CloneMeshFVF( D3DXMESH_MANAGED, ( inputVal->importedMesh )->GetFVF() | D3DFVF_NORMAL, D3dDevice, &pTempMesh );
+
+		D3DXComputeNormals( pTempMesh, 0 );
+		
+		( inputVal->importedMesh )->Release();
+		( inputVal->importedMesh ) = pTempMesh;
+	}
+
+	D3DXMATERIAL* d3dxMarteials = (D3DXMATERIAL*) D3dxMtrialBuffer->GetBufferPointer();
+	inputVal->MeshMarterials = new D3DMATERIAL9[inputVal->NumMaterials];
+	if ( nullptr == inputVal->MeshMarterials)
+	{
+		return E_OUTOFMEMORY;
+	}
+
+	inputVal->MeshTexture = new LPDIRECT3DTEXTURE9[inputVal->NumMaterials];
+	if (nullptr == inputVal->MeshTexture)
+	{
+		return E_OUTOFMEMORY;
+	}
+
+	for ( DWORD i = 0; i < inputVal->NumMaterials; ++i )
+	{
+		inputVal->MeshMarterials[i] = d3dxMarteials[i].MatD3D;
+
+		inputVal->MeshMarterials[i].Ambient = inputVal->MeshMarterials[i].Diffuse;
+
+		inputVal->MeshTexture[i] = NULL;
+		if ( ( NULL != d3dxMarteials[i].pTextureFilename ) && lstrlenA( d3dxMarteials[i].pTextureFilename )>0 )
+		{
+			if ( FAILED( D3DXCreateTextureFromFileA( D3dDevice, d3dxMarteials[i].pTextureFilename, &(inputVal->MeshTexture[i]) ) ) )
+			{
+				const CHAR* strPrefix = "..\\";
+				CHAR strTexture[MAX_PATH];
+				strcpy_s( strTexture, MAX_PATH, strPrefix );
+				strcat_s( strTexture, MAX_PATH, d3dxMarteials[i].pTextureFilename );
+
+				if ( FAILED( D3DXCreateTextureFromFileA( D3dDevice, strTexture, &(inputVal->MeshTexture[i]) ) ) )
+				{
+					MessageBox( NULL, L"Could not find texture map", L"Meshes.exe", MB_OK );
+				}
+			}
+		}
+	}
+	
+
+	D3dxMtrialBuffer->Release();
+
+	return S_OK;
+}
+
+void SetupTranslateMatrices( float moveX, float moveY, float moveZ )
+{
+	D3DXMATRIXA16 matWorld;
+	D3DXMatrixIdentity( &matWorld );
+	D3dDevice->SetTransform( D3DTS_WORLD, &matWorld );
+
+	//x, y, z축 입력 값에 대해 이동 처리
+	D3DXMATRIXA16 thisMatrix;
+	D3DXMatrixTranslation( &thisMatrix, moveX, moveY, moveZ );
+	D3dDevice->MultiplyTransform( D3DTS_WORLD, &thisMatrix );
+
+	//향후 추가 매트릭스 처리 필요 내용에 대해 추가 예정
+}
+
+//카메라 세팅은 향후에 진행하도록 함
+//default로 일단 이렇게 둠
+void ViewSetting()
+{
+	D3DXVECTOR3 vEyePt( 0.f, 3.f, -5.f );
+	D3DXVECTOR3 vLookatPt( 0.f, 0.f, 0.f );
+	D3DXVECTOR3 vUpVec( 0.f, 1.f, 0.f );
+	D3DXMATRIXA16 matView;
+	D3DXMatrixLookAtLH( &matView, &vEyePt, &vLookatPt, &vUpVec );
+	D3dDevice->SetTransform( D3DTS_VIEW, &matView );
+
+	D3DXMATRIXA16 matProj;
+	D3DXMatrixPerspectiveFovLH( &matProj, D3DX_PI / 2, 1.0f, 1.0f, 100.0f );
+	D3dDevice->SetTransform( D3DTS_PROJECTION, &matProj );
+}
+
+//light 세팅에 대해서는 향후 추가
+//light는 여러개를 미리 가지고 있게 할 것인가?
+//아니면 사용자가 최소로 추가하고 해당 light를 공용 자원으로 할 것인가?
+
+//////////////////////////////////////////////////////////////////////////
+//render에 인자를 넣으면 전부 실행되도록 함
+//////////////////////////////////////////////////////////////////////////
+YAMANGDXDLL_API void Render( MESHOBJECT* inputVal, float moveX, float moveY, float moveZ )
+{
+	if (SUCCEEDED(D3dDevice->BeginScene()))
+	{
+		SetupTranslateMatrices( moveX, moveY, moveZ );
+		ViewSetting();
+		//lightsetting
+
+		D3dDevice->SetTextureStageState( 0, D3DTSS_COLOROP, D3DTOP_MODULATE );
+		D3dDevice->SetTextureStageState( 0, D3DTSS_COLORARG1, D3DTA_TEXTURE );
+		D3dDevice->SetTextureStageState( 0, D3DTSS_COLORARG2, D3DTA_DIFFUSE );
+		D3dDevice->SetTextureStageState( 0, D3DTSS_ALPHAOP, D3DTOP_DISABLE );
+
+		for ( DWORD i = 0; i < inputVal->NumMaterials; ++i )
+		{
+			D3dDevice->SetMaterial( &inputVal->MeshMarterials[i] );
+			D3dDevice->SetTexture( 0, inputVal->MeshTexture[i] );
+
+			(inputVal->importedMesh)->DrawSubset( i );
+		}
+
+		D3dDevice->EndScene();
+	}
+
+	D3dDevice->Present( NULL, NULL, NULL, NULL );
+}
+
+YAMANGDXDLL_API void MeshObjectCleanUp( MESHOBJECT* inputVal )
+{
+	if ( NULL != inputVal->MeshMarterials)
+	{
+		delete[] inputVal->MeshMarterials;
+	}
+	if ( inputVal->MeshTexture )
+	{
+		for ( DWORD i = 0; i < inputVal->NumMaterials; ++i )
+		{
+			if ( inputVal->MeshTexture[i] )
+			{
+				inputVal->MeshTexture[i]->Release();
+			}
+		}
+		delete[] inputVal->MeshTexture;
+	}
+	if ( NULL != inputVal->importedMesh )
+	{
+		(inputVal->importedMesh)->Release();
+	}
+}
+
+YAMANGDXDLL_API void D3DCleanUp()
+{
+	if ( NULL != D3dDevice )
+	{
+		D3dDevice->Release();
+	}
+
+	if ( NULL != D3D )
+	{
+		D3D->Release();
+	}
+}
+
+
+
+
+
+// 내보낸 변수의 예제입니다.
+// YAMANGDXDLL_API int nyaMangDxDll=0;
---
+++ YaMang/yaMangDxDll/yaMangDxDll/yaMangDxDll.h
@@ -0,0 +1,53 @@
+// 다음 ifdef 블록은 DLL에서 내보내기하는 작업을 쉽게 해 주는 매크로를 만드는 
+// 표준 방식입니다. 이 DLL에 들어 있는 파일은 모두 명령줄에 정의된 _EXPORTS 기호로
+// 컴파일되며, 다른 프로젝트에서는 이 기호를 정의할 수 없습니다.
+// 이렇게 하면 소스 파일에 이 파일이 들어 있는 다른 모든 프로젝트에서는 
+// YAMANGDXDLL_API 함수를 DLL에서 가져오는 것으로 보고, 이 DLL은
+// 이 DLL은 해당 매크로로 정의된 기호가 내보내지는 것으로 봅니다.
+#ifdef YAMANGDXDLL_EXPORTS
+#define YAMANGDXDLL_API __declspec(dllexport)
+#else
+#define YAMANGDXDLL_API __declspec(dllimport)
+#endif
+
+#include <d3dx9.h>
+#include <iostream>
+
+//////////////////////////////////////////////////////////////////////////
+// return struct
+// 함수를 부를 경우 해당 struct를 반환해 내부를 사용할 수 있도록 함
+//////////////////////////////////////////////////////////////////////////
+struct MESHOBJECT
+{
+	//vertexbuffer를 직접 만들어서 사용하는 경우는 없을 것이므로 삭제
+	//LPDIRECT3DVERTEXBUFFER9 VertexBuffer = nullptr;
+
+	LPD3DXMESH importedMesh = nullptr;
+	D3DMATERIAL9* MeshMarterials = nullptr;
+	LPDIRECT3DTEXTURE9* MeshTexture = nullptr;
+
+	DWORD NumMaterials = 0L;
+};
+
+extern "C"{
+	YAMANGDXDLL_API HRESULT InitD3D( HWND hWnd );
+	YAMANGDXDLL_API HRESULT InitGeometry( HWND hWnd, LPCTSTR fileName, MESHOBJECT* inputVal );
+	
+	//x,y,z축 이동이 없으면 0을 넣으면 됩니다.
+	YAMANGDXDLL_API void Render( MESHOBJECT* inputVal, float moveX, float moveY, float moveZ );
+
+	//각 object 해제시 사용
+	YAMANGDXDLL_API void MeshObjectCleanUp( MESHOBJECT* inputVal );
+
+	//최종 1회 실행
+	YAMANGDXDLL_API void D3DCleanUp();
+	
+	//내부에서 처리하도록 변경
+	//YAMANGDXDLL_API void SetupTranslateMatrices(float moveX, float moveY, float moveZ);
+	//YAMANGDXDLL_API void ViewSetting();
+}
+
+//extern YAMANGDXDLL_API int nyaMangDxDll;
+
+
+
