1b1a2f3d823538cc3ed7e37fa7281b1c33fcc329
nhnponyo <coponyo@naver.com>
2014-04-07 04:38:10 +0900

    Tool First Commit
    InitGeometry 호랑이 했으나 화면에 호랑이가 없다!
    아직 clean 이 없어서 메모리릭이 걱정되는 상태 ~_~

---
+++ YaMang/Tool/CharacterTool/CharacterTool/Main.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace CharacterTool
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+
+            InitializeComponent();
+            YamangDll.InitD3D(this.Window.Handle);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "D3D Mesh File (.x)|*.x|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1; 
+            openFileDialog1.RestoreDirectory = true; 
+            openFileDialog1.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Open X File
+
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            YamangDll.MESHOBJECT info = new YamangDll.MESHOBJECT();
+            IntPtr infoPtr = Marshal.AllocHGlobal(Marshal.SizeOf(info));
+            Marshal.StructureToPtr(info, infoPtr, false);
+            info = (YamangDll.MESHOBJECT)Marshal.PtrToStructure(infoPtr, typeof(YamangDll.MESHOBJECT));
+            
+            string filename = "tiger.x";
+            YamangDll.InitGeometry(this.Window.Handle, filename, ref infoPtr );
+            YamangDll.Render(ref infoPtr, 0, 0, 0);
+        }
+    }
+}
---
+++ YaMang/Tool/CharacterTool/CharacterTool/YamangDll.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace CharacterTool
+{
+    class YamangDll
+    {
+        ////////////////////////////////////////////////////////////
+        // C++ 타입을 common language로 C#과 호환되도록 하기 - 
+        // http://msdn.microsoft.com/en-us/library/Aa712982
+        /// ////////////////////////////////////////////////////////
+
+        [DllImport("yaMangDxDll.dll", CallingConvention = CallingConvention.Cdecl )]
+        public static extern Int32 InitD3D( IntPtr hWnd );
+
+        [DllImport("yaMangDxDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void D3DCleanUp();
+
+        [DllImport("yaMangDxDll.dll",  CharSet = CharSet.Unicode , CallingConvention = CallingConvention.Cdecl)]
+        public static extern Int32 InitGeometry( IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)] String fileName, ref IntPtr inputVal);
+
+        [DllImport("yaMangDxDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Render( ref IntPtr inputVal, float moveX, float moveY, float moveZ);
+
+        [DllImport("yaMangDxDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void MeshObjectCleanUp( IntPtr inputVal );
+
+        //DLL 내부 MESHOBJECT 정의
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct MESHOBJECT
+        {
+            IntPtr importedMesh;
+            IntPtr MeshMarterials;
+            IntPtr MeshTexture;
+
+            UInt32 NumMaterials;
+        };
+
+    }
+}
