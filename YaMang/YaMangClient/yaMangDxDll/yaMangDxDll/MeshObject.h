565bd9cc3c713cc192e049fd61758c4d28a45c46
zrma <bulbitain@nhnnext.org>
2014-04-09 02:30:05 +0900

    초기화 코드 다시 재삽입 @_@
    ㅇㅅㅇ

--- YaMang/YaMangClient/yaMangDxDll/yaMangDxDll/MeshObject.h
+++ YaMang/YaMangClient/yaMangDxDll/yaMangDxDll/MeshObject.h
@@ -9,9 +9,9 @@ struct MESHOBJECT
 	//vertexbuffer를 직접 만들어서 사용하는 경우는 없을 것이므로 삭제
 	//LPDIRECT3DVERTEXBUFFER9 VertexBuffer = nullptr;
 
-	LPD3DXMESH			importedMesh;
-	D3DMATERIAL9*		MeshMarterials;
-	LPDIRECT3DTEXTURE9*	MeshTexture;
+	LPD3DXMESH			importedMesh = nullptr;
+	D3DMATERIAL9*		MeshMarterials = nullptr;
+	LPDIRECT3DTEXTURE9*	MeshTexture = nullptr;
 	
-	DWORD				NumMaterials;
+	DWORD				NumMaterials = 0L;
 };
