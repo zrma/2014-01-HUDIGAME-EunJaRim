b31e8cc1b27f63b423e3c612fa8e83c9105cc76d
zrma <bulbitain@nhnnext.org>
2014-04-11 00:35:26 +0900

    코딩 컨벤션에 따른 변수명 변경
    ㅇㅅㅇ

--- YaMang/YaMangClient/yaMangDxDll/yaMangDxDll/yaMangDxDll.cpp
+++ YaMang/YaMangClient/yaMangDxDll/yaMangDxDll/yaMangDxDll.cpp
@@ -10,8 +10,8 @@
 //Global val 선언
 //////////////////////////////////////////////////////////////////////////
 //부모 Process 공용 자원
-LPDIRECT3D9 D3D = nullptr;
-LPDIRECT3DDEVICE9 D3dDevice = nullptr;
+LPDIRECT3D9 g_D3D = nullptr;
+LPDIRECT3DDEVICE9 g_D3dDevice = nullptr;
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -22,7 +22,7 @@ LPDIRECT3DDEVICE9 D3dDevice = nullptr;
 //////////////////////////////////////////////////////////////////////////
 YAMANGDXDLL_API HRESULT InitD3D( HWND hWnd )
 {
-	if ( nullptr == ( D3D = Direct3DCreate9( D3D_SDK_VERSION ) ) )
+	if ( nullptr == ( g_D3D = Direct3DCreate9( D3D_SDK_VERSION ) ) )
 	{
 		// 오류
 		MessageBox( NULL, L"Could not Create D3D", L"Meshes.exe", MB_OK );
@@ -37,14 +37,14 @@ YAMANGDXDLL_API HRESULT InitD3D( HWND hWnd )
 	d3dpp.EnableAutoDepthStencil = TRUE;
 	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
 
-	if ( FAILED (D3D->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &D3dDevice ) ) )
+	if ( FAILED( g_D3D->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &g_D3dDevice ) ) )
 	{
 		MessageBox( NULL, L"Could not CreateDevice", L"Meshes.exe", MB_OK );
 		return E_FAIL;
 	}
 
-	D3dDevice->SetRenderState( D3DRS_CULLMODE, D3DCULL_NONE );
-	D3dDevice->SetRenderState( D3DRS_ZENABLE, TRUE );
+	g_D3dDevice->SetRenderState( D3DRS_CULLMODE, D3DCULL_NONE );
+	g_D3dDevice->SetRenderState( D3DRS_ZENABLE, TRUE );
 
 #ifdef _PRINT_CONSOLE
 	Logger::GetInstance();
@@ -58,7 +58,7 @@ YAMANGDXDLL_API HRESULT InitGeometry( HWND hWnd, LPCTSTR fileName, MESHOBJECT* i
 	LPD3DXBUFFER D3dxMtrialBuffer;
 
 	//x file import
-	if (FAILED(D3DXLoadMeshFromX(fileName, D3DXMESH_SYSTEMMEM, D3dDevice, NULL, &D3dxMtrialBuffer, NULL, &(inputVal->NumMaterials), &inputVal->importedMesh)))
+	if ( FAILED( D3DXLoadMeshFromX( fileName, D3DXMESH_SYSTEMMEM, g_D3dDevice, NULL, &D3dxMtrialBuffer, NULL, &( inputVal->NumMaterials ), &inputVal->importedMesh ) ) )
 	{
 		MessageBox( NULL, L"Could not find x file", L"Mesh Load", MB_OK );
 		return E_FAIL;
@@ -68,23 +68,23 @@ YAMANGDXDLL_API HRESULT InitGeometry( HWND hWnd, LPCTSTR fileName, MESHOBJECT* i
 	if ( !( ( inputVal->importedMesh )->GetFVF()&D3DFVF_NORMAL ) )
 	{
 		ID3DXMesh* pTempMesh = 0;
-		( inputVal->importedMesh )->CloneMeshFVF( D3DXMESH_MANAGED, ( inputVal->importedMesh )->GetFVF() | D3DFVF_NORMAL, D3dDevice, &pTempMesh );
+		( inputVal->importedMesh )->CloneMeshFVF( D3DXMESH_MANAGED, ( inputVal->importedMesh )->GetFVF() | D3DFVF_NORMAL, g_D3dDevice, &pTempMesh );
 
 		D3DXComputeNormals( pTempMesh, 0 );
-		
+
 		( inputVal->importedMesh )->Release();
 		( inputVal->importedMesh ) = pTempMesh;
 	}
 
-	D3DXMATERIAL* d3dxMarteials = (D3DXMATERIAL*) D3dxMtrialBuffer->GetBufferPointer();
+	D3DXMATERIAL* d3dxMarteials = (D3DXMATERIAL*)D3dxMtrialBuffer->GetBufferPointer();
 	inputVal->MeshMarterials = new D3DMATERIAL9[inputVal->NumMaterials];
-	if ( nullptr == inputVal->MeshMarterials)
+	if ( nullptr == inputVal->MeshMarterials )
 	{
 		return E_OUTOFMEMORY;
 	}
 
 	inputVal->MeshTexture = new LPDIRECT3DTEXTURE9[inputVal->NumMaterials];
-	if (nullptr == inputVal->MeshTexture)
+	if ( nullptr == inputVal->MeshTexture )
 	{
 		return E_OUTOFMEMORY;
 	}
@@ -98,21 +98,21 @@ YAMANGDXDLL_API HRESULT InitGeometry( HWND hWnd, LPCTSTR fileName, MESHOBJECT* i
 		inputVal->MeshTexture[i] = NULL;
 		if ( ( NULL != d3dxMarteials[i].pTextureFilename ) && lstrlenA( d3dxMarteials[i].pTextureFilename )>0 )
 		{
-			if ( FAILED( D3DXCreateTextureFromFileA( D3dDevice, d3dxMarteials[i].pTextureFilename, &(inputVal->MeshTexture[i]) ) ) )
+			if ( FAILED( D3DXCreateTextureFromFileA( g_D3dDevice, d3dxMarteials[i].pTextureFilename, &( inputVal->MeshTexture[i] ) ) ) )
 			{
 				const CHAR* strPrefix = "..\\";
 				CHAR strTexture[MAX_PATH];
 				strcpy_s( strTexture, MAX_PATH, strPrefix );
 				strcat_s( strTexture, MAX_PATH, d3dxMarteials[i].pTextureFilename );
 
-				if ( FAILED( D3DXCreateTextureFromFileA( D3dDevice, strTexture, &(inputVal->MeshTexture[i]) ) ) )
+				if ( FAILED( D3DXCreateTextureFromFileA( g_D3dDevice, strTexture, &( inputVal->MeshTexture[i] ) ) ) )
 				{
 					MessageBox( NULL, L"Could not find texture map", L"Meshes.exe", MB_OK );
 				}
 			}
 		}
 	}
-	
+
 
 	D3dxMtrialBuffer->Release();
 
@@ -123,12 +123,12 @@ void SetupTranslateMatrices( float moveX, float moveY, float moveZ )
 {
 	D3DXMATRIXA16 matWorld;
 	D3DXMatrixIdentity( &matWorld );
-	D3dDevice->SetTransform( D3DTS_WORLD, &matWorld );
+	g_D3dDevice->SetTransform( D3DTS_WORLD, &matWorld );
 
 	//x, y, z축 입력 값에 대해 이동 처리
 	D3DXMATRIXA16 thisMatrix;
 	D3DXMatrixTranslation( &thisMatrix, moveX, moveY, moveZ );
-	D3dDevice->MultiplyTransform( D3DTS_WORLD, &thisMatrix );
+	g_D3dDevice->MultiplyTransform( D3DTS_WORLD, &thisMatrix );
 
 	//향후 추가 매트릭스 처리 필요 내용에 대해 추가 예정
 }
@@ -142,17 +142,17 @@ void ViewSetting()
 	D3DXVECTOR3 vUpVec( 0.f, 1.f, 0.f );
 	D3DXMATRIXA16 matView;
 	D3DXMatrixLookAtLH( &matView, &vEyePt, &vLookatPt, &vUpVec );
-	D3dDevice->SetTransform( D3DTS_VIEW, &matView );
+	g_D3dDevice->SetTransform( D3DTS_VIEW, &matView );
 
 	D3DXMATRIXA16 matProj;
 	D3DXMatrixPerspectiveFovLH( &matProj, D3DX_PI / 2, 1.0f, 1.0f, 100.0f );
-	D3dDevice->SetTransform( D3DTS_PROJECTION, &matProj );
+	g_D3dDevice->SetTransform( D3DTS_PROJECTION, &matProj );
 }
 
 //light 세팅에 대해서는 향후 추가
 //light는 여러개를 미리 가지고 있게 할 것인가?
 //아니면 사용자가 최소로 추가하고 해당 light를 공용 자원으로 할 것인가?
-void Lighting(int lightNum)
+void Lighting( int lightNum )
 {
 	//재질 속성 부여
 	D3DMATERIAL9 mtrl;
@@ -161,7 +161,7 @@ void Lighting(int lightNum)
 	mtrl.Diffuse.g = mtrl.Ambient.g = 1.0f;
 	mtrl.Diffuse.b = mtrl.Ambient.b = 1.0f;
 	mtrl.Diffuse.a = mtrl.Ambient.a = 1.0f;
-	D3dDevice->SetMaterial( &mtrl );
+	g_D3dDevice->SetMaterial( &mtrl );
 
 	D3DXVECTOR3 vecDir0;
 	D3DLIGHT9 light0;
@@ -171,7 +171,7 @@ void Lighting(int lightNum)
 	light0.Diffuse.g = 1.f;
 	light0.Diffuse.b = 1.f;
 	vecDir0 = D3DXVECTOR3( -1.f, 1.f, 0.f );
-	D3DXVec3Normalize( (D3DXVECTOR3*) &light0.Direction, &vecDir0 );
+	D3DXVec3Normalize( (D3DXVECTOR3*)&light0.Direction, &vecDir0 );
 	light0.Range = 1000.f;
 
 
@@ -183,28 +183,28 @@ void Lighting(int lightNum)
 	light1.Diffuse.g = 1.f;
 	light1.Diffuse.b = 1.f;
 	vecDir1 = D3DXVECTOR3( 1.f, 1.f, 0.f );
-	D3DXVec3Normalize( (D3DXVECTOR3*) &light1.Direction, &vecDir1 );
+	D3DXVec3Normalize( (D3DXVECTOR3*)&light1.Direction, &vecDir1 );
 	light1.Range = 1000.f;
 
-	D3dDevice->SetLight( 0, &light0 );
-	D3dDevice->SetLight( 1, &light1 );
-		
+	g_D3dDevice->SetLight( 0, &light0 );
+	g_D3dDevice->SetLight( 1, &light1 );
+
 	switch ( lightNum )
 	{
 		case 1:
-			D3dDevice->LightEnable( 0, TRUE );
-			D3dDevice->LightEnable( 1, FALSE );
+			g_D3dDevice->LightEnable( 0, TRUE );
+			g_D3dDevice->LightEnable( 1, FALSE );
 			break;
 		case 2:
-			D3dDevice->LightEnable( 0, FALSE );
-			D3dDevice->LightEnable( 1, TRUE );
+			g_D3dDevice->LightEnable( 0, FALSE );
+			g_D3dDevice->LightEnable( 1, TRUE );
 			break;
 		default:
 			break;
 	}
 
-	D3dDevice->SetRenderState( D3DRS_LIGHTING, TRUE );
-	D3dDevice->SetRenderState( D3DRS_AMBIENT, 0x00808080 );
+	g_D3dDevice->SetRenderState( D3DRS_LIGHTING, TRUE );
+	g_D3dDevice->SetRenderState( D3DRS_AMBIENT, 0x00808080 );
 }
 
 
@@ -216,17 +216,17 @@ void Lighting(int lightNum)
 //////////////////////////////////////////////////////////////////////////
 YAMANGDXDLL_API bool PreRendering( float moveX, float moveY, float moveZ )
 {
-	if ( NULL == D3dDevice )
+	if ( NULL == g_D3dDevice )
 	{
 		return false;
 	}
 
-	D3dDevice->Clear( 0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB( 30, 10, 10 ), 1.0f, 0 );
+	g_D3dDevice->Clear( 0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB( 30, 10, 10 ), 1.0f, 0 );
 
 	bool flag = false;
 	//렌더 방어코드
 	//pre rendering 단계에서 진행되지 않으면 향후 render 모두 실패
-	if ( SUCCEEDED( D3dDevice->BeginScene() ) )
+	if ( SUCCEEDED( g_D3dDevice->BeginScene() ) )
 	{
 		SetupTranslateMatrices( moveX, moveY, moveZ );
 		ViewSetting();
@@ -234,17 +234,17 @@ YAMANGDXDLL_API bool PreRendering( float moveX, float moveY, float moveZ )
 		//일단 1로 진행, 향후 라이트 개수 등 확정되면 인자 받아 설정
 		int lightNum = 1;
 		Lighting( lightNum );
-		
+
 		//Log( "라이팅 세팅!\n" );
 
-		D3dDevice->SetTextureStageState( 0, D3DTSS_COLOROP, D3DTOP_MODULATE );
-		D3dDevice->SetTextureStageState( 0, D3DTSS_COLORARG1, D3DTA_TEXTURE );
-		D3dDevice->SetTextureStageState( 0, D3DTSS_COLORARG2, D3DTA_DIFFUSE );
-		D3dDevice->SetTextureStageState( 0, D3DTSS_ALPHAOP, D3DTOP_DISABLE );
-		
+		g_D3dDevice->SetTextureStageState( 0, D3DTSS_COLOROP, D3DTOP_MODULATE );
+		g_D3dDevice->SetTextureStageState( 0, D3DTSS_COLORARG1, D3DTA_TEXTURE );
+		g_D3dDevice->SetTextureStageState( 0, D3DTSS_COLORARG2, D3DTA_DIFFUSE );
+		g_D3dDevice->SetTextureStageState( 0, D3DTSS_ALPHAOP, D3DTOP_DISABLE );
+
 		//Log( "Render Begin \n" );
 		//Log( "pre render 완료!\n" );
-		
+
 		flag = true;
 	}
 
@@ -256,8 +256,8 @@ YAMANGDXDLL_API void Rendering( MESHOBJECT* inputVal )
 	//Log( "Now Render : %p \n", inputVal );
 	for ( DWORD i = 0; i < inputVal->NumMaterials; ++i )
 	{
-		D3dDevice->SetMaterial( &inputVal->MeshMarterials[i] );
-		D3dDevice->SetTexture( 0, inputVal->MeshTexture[i] );
+		g_D3dDevice->SetMaterial( &inputVal->MeshMarterials[i] );
+		g_D3dDevice->SetTexture( 0, inputVal->MeshTexture[i] );
 
 		( inputVal->importedMesh )->DrawSubset( i );
 	}
@@ -265,10 +265,10 @@ YAMANGDXDLL_API void Rendering( MESHOBJECT* inputVal )
 
 YAMANGDXDLL_API void PostRendering()
 {
-	D3dDevice->EndScene();
+	g_D3dDevice->EndScene();
 
 	//Log( "Render End \n" );
-	D3dDevice->Present( NULL, NULL, NULL, NULL );
+	g_D3dDevice->Present( NULL, NULL, NULL, NULL );
 }
 		
 
@@ -298,14 +298,14 @@ YAMANGDXDLL_API void MeshObjectCleanUp( MESHOBJECT* inputVal )
 
 YAMANGDXDLL_API void D3DCleanUp()
 {
-	if ( NULL != D3dDevice )
+	if ( NULL != g_D3dDevice )
 	{
-		D3dDevice->Release();
+		g_D3dDevice->Release();
 	}
 
-	if ( NULL != D3D )
+	if ( NULL != g_D3D )
 	{
-		D3D->Release();
+		g_D3D->Release();
 	}
 
 #ifdef _PRINT_CONSOLE
