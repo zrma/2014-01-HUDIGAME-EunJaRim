6164c57b3e353368b845cb66f3212e8e4dd145b7
zrma <bulbitain@nhnnext.org>
2014-04-11 10:18:43 +0900

    윈도우 메시지 처리 중 소켓 이벤트 처리 @_@

--- YaMang/YaMangClient/YaMangClient/YaMangClient/NetworkManager.cpp
+++ YaMang/YaMangClient/YaMangClient/YaMangClient/NetworkManager.cpp
@@ -6,6 +6,7 @@
 #include "tinyxml.h"
 #include "xpath_static.h"
 #include <string>
+#include <time.h>
 
 #pragma comment(lib,"ws2_32.lib")
 
@@ -186,4 +187,110 @@ void NetworkManager::ProcessPacket()
 		default:
 			assert( false );
 	}
+}
+
+bool NetworkManager::Connected( WPARAM wParam, LPARAM lParam )
+{
+	// lParam 이 에러인지 검출 해보기
+	if ( WSAGETSELECTERROR( lParam ) )
+	{
+		MessageBox( MainWindow::GetInstance()->Window(), L"WSAGETSELECTERROR", L"Error", MB_OK | MB_ICONERROR );
+		SendMessage( MainWindow::GetInstance()->Window(), WM_DESTROY, NULL, NULL );
+		return false;
+	}
+
+	// 에러 아니면 이벤트 검출해서 switch
+	switch ( WSAGETSELECTEVENT( lParam ) )
+	{
+		case FD_CONNECT:
+			// 연결이 되었다
+		{
+			/// NAGLE 끈다
+			int opt = 1;
+			setsockopt( m_Socket, IPPROTO_TCP, TCP_NODELAY, (const char*)&opt, sizeof( int ) );
+
+			srand( static_cast<unsigned int> ( time( NULL ) ) );
+			/// 대략 1000~1100 의 ID로 로그인 해보자
+			LoginRequest sendData;
+			sendData.mPlayerId = 1000 + rand() % 101;
+
+			if ( m_SendBuffer.Write( (const char*)&sendData, sendData.mSize ) )
+			{
+				PostMessage( MainWindow::GetInstance()->Window(), WM_SOCKET, wParam, FD_WRITE );
+			}
+
+
+			int nResult = WSAAsyncSelect( m_Socket, MainWindow::GetInstance()->Window(), WM_SOCKET, ( FD_CLOSE | FD_READ | FD_WRITE ) );
+			if ( nResult )
+			{
+				assert( false );
+				break;
+			}
+		}
+			break;
+
+		case FD_READ:
+		{
+			char inBuf[4096] = { 0, };
+
+			int recvLen = recv( m_Socket, inBuf, 4096, 0 );
+			// send() 함수와 반대
+
+			// 소켓에서 읽어온 데이터를 일단 버퍼에 쓰자
+			if ( !m_RecvBuffer.Write( inBuf, recvLen ) )
+			{
+				/// 버퍼 꽉찼다.
+				assert( false );
+			}
+
+			ProcessPacket();
+			//////////////////////////////////////////////////////////////////////////
+			// 패킷 핸들링!
+			//////////////////////////////////////////////////////////////////////////
+		}
+			break;
+			//////////////////////////////////////////////////////////////////////////
+			// 데이터를 받으면 -> 버퍼에 쓴 후에, 핸들링 하는 쪽에서 버퍼 데이터 뽑아서 처리
+			//
+			// 데이터를 보낼때 -> 버퍼에 쓴 후에, FD_WRITE 쪽에서 버퍼 데이터 뽑아서 send()
+			//////////////////////////////////////////////////////////////////////////
+
+		case FD_WRITE:
+		{
+			/// 실제로 버퍼에 있는것들 꺼내서 보내기
+			int size = m_SendBuffer.GetCurrentSize();
+			if ( size > 0 )
+			{
+				char* data = new char[size];
+				m_SendBuffer.Peek( data );
+
+				int sent = send( m_Socket, data, size, 0 );
+
+				/// 다를수 있다
+				if ( sent != size )
+				{
+					OutputDebugStringA( "sent != request\n" );
+				}
+
+				// 보낸 데이터는 지우자
+				m_SendBuffer.Consume( sent );
+
+				delete[] data;
+			}
+
+		}
+			break;
+
+		case FD_CLOSE:
+		{
+			MessageBox( MainWindow::GetInstance()->Window(), L"Server closed connection", L"Connection closed!", MB_ICONINFORMATION | MB_OK );
+			closesocket( m_Socket );
+			SendMessage( MainWindow::GetInstance()->Window(), WM_DESTROY, NULL, NULL );
+
+			// 여기는 네트워크 매니저에서 Destroy 하고 매니저를 Release 하도록 한다
+		}
+			break;
+	}
+
+	return true;
 }
