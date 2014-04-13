f3a4343ac5162a91e233031d42175d89acca456b
zrma <bulbitain@nhnnext.org>
2014-04-14 00:17:21 +0900

    살짝 손봄 - InvalidateRect를 하단으로.
    ㅇㅅㅇ

--- YaMang/YaMangClient/YaMangClient/YaMangClient/MainWindow.cpp
+++ YaMang/YaMangClient/YaMangClient/YaMangClient/MainWindow.cpp
@@ -40,18 +40,13 @@ LRESULT MainWindow::HandleMessage( UINT uMsg, WPARAM wParam, LPARAM lParam )
 
 		case WM_SIZE:
 		{
-			// 리사이징에 대한 부분을 여기서 처리 해 줌
-			// int x = (int)LOWORD( lParam );
-			// int y = (int)HIWORD( lParam );
-			// Renderer::GetInstance()->Resize( x, y );
-			InvalidateRect( m_HandleOfWindow, NULL, FALSE );
-
 			RECT rect;
 			GetWindowRect( m_HandleOfWindow, &rect );
 			float width = rect.right - rect.left;
 			float height = rect.bottom - rect.left;
 			
 			Renderer::GetInstance()->ResizeWindow( width, height );
+			InvalidateRect( m_HandleOfWindow, NULL, FALSE );
 		}
 			return 0;
 
