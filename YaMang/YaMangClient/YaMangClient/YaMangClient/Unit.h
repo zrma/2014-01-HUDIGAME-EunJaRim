0ef9aa6793c66066d078498b8ee9414c85ad8d0b
zrma <bulbitain@nhnnext.org>
2014-04-17 22:02:01 +0900

    유닛 추가중

--- YaMang/YaMangClient/YaMangClient/YaMangClient/Unit.h
+++ YaMang/YaMangClient/YaMangClient/YaMangClient/Unit.h
@@ -1,8 +1,17 @@
 ﻿#pragma once
+
+extern const float NOT_A_NUMBER_FLOAT;
+
 class Unit
 {
 public:
 	Unit();
 	virtual ~Unit();
-};
 
+private:
+	float	m_PosX = NOT_A_NUMBER_FLOAT;
+	float	m_PosY = -NOT_A_NUMBER_FLOAT;
+	int		m_UnitId = -1;
+
+	int		m_MoveSpeed = 0;
+};
