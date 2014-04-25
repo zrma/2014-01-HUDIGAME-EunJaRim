0b06c82ca1c76504ff7d23a6ebdba4c6b4e2f82e
zrma <bulbitain@nhnnext.org>
2014-04-26 02:04:19 +0900

    주석을 적어보았다.
    ㅇㅅㅇ 과연 며칠이나 갈련지...

--- YaMang/YaMangClient/YaMangClient/YaMangClient/Action.h
+++ YaMang/YaMangClient/YaMangClient/YaMangClient/Action.h
@@ -1,13 +1,25 @@
-﻿#pragma once
+﻿/**
+*@file		Action.h
+*@brief		Corps가 갖는 단일 행동을 정의합니다.
+*@details	Corps의 Update() 내에서 호출(무한루프) 됩니다.
+*/
+
+#pragma once
 #include "Logger.h"
 #include "EnumSet.h"
 #include "Corps.h"
 #include "Act.h"
 
+/**
+*@brief		Action 추상 클래스
+*@details	모든 Action은 이 함수 객체(struct)를 상속 받아 구현합니다.
+*@author	zrma
+*@date		2014-04-25
+*/
 struct Action
 {
 public:
-	virtual void operator()( Corps& ) {}
+	virtual void operator()( Corps& ) = 0;
 };
 
 struct ActionDefault: public Action
