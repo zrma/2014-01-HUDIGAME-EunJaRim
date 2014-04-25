fbf85b3d39cf10047a6a372a249d8ad8dbbeb9a0
zrma <bulbitain@nhnnext.org>
2014-04-25 11:02:57 +0900

    Action Functor를 이용하려고 하는 첫걸음이 시작되므니다.

--- YaMang/YaMangClient/YaMangClient/YaMangClient/Action.h
+++ YaMang/YaMangClient/YaMangClient/YaMangClient/Action.h
@@ -1,17 +1,49 @@
 ﻿#pragma once
-class Action
+
+class Corps;
+
+struct Action
 {
 public:
-	Action();
-	virtual ~Action();
-
-	virtual void	OnBegin() = 0;
-	virtual void	OnTick() = 0;
-	virtual void	OnEnd() = 0;
-
-	void	Stop() { m_IsPossible = false; }
-
-private:
-	bool	m_IsPossible = false;
+	virtual void operator()( const Corps& ) = 0;
 };
 
+struct Attack: public Action
+{
+
+};
+
+struct CorpsSpawn: public Action
+{
+
+};
+
+struct DefenseFormation: public Action
+{
+
+};
+
+struct DestroyFormation: public Action
+{
+
+};
+
+struct HoldPosition: public Action
+{
+
+};
+
+struct MovePosition: public Action
+{
+
+};
+
+struct RushFormation: public Action
+{
+
+};
+
+struct TakeArea: public Action
+{
+
+};
