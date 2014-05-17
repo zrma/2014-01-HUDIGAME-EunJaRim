ccb7faab524c50c216fcf750d17880d020853171
zrma <bulbitain@nhnnext.org>
2014-05-17 21:36:06 +0900

    테스트를 위해서 일단 오버랩드 윈도우로 다시 설정
    ㅇㅅㅇ

--- YaMang/YaMangClient/YaMangClient/YaMangClient/YaMangClient.cpp
+++ YaMang/YaMangClient/YaMangClient/YaMangClient/YaMangClient.cpp
@@ -38,7 +38,7 @@ int APIENTRY _tWinMain( _In_ HINSTANCE hInstance,
 
 	// WS_POPUPWINDOW
 	// WS_OVERLAPPEDWINDOW
-	if ( false == MainWindow::GetInstance()->Create( L"YaMang", WS_POPUPWINDOW ) )
+	if ( false == MainWindow::GetInstance()->Create( L"YaMang", WS_OVERLAPPEDWINDOW ) )
 	{
 		return 0;
 	}
