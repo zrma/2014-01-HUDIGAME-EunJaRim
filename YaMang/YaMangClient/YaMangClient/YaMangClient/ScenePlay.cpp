b87b3d6d3de8a32c93f8d8d81e5f5f1f21f80c59
nhnponyo <xprponyo@nhnnext.org>
2014-05-20 21:24:52 +0900

    임시로 띄운 맵 위치를 변경하고 리젠바 앞에 보이게 함
    좋은 이미지 없나

--- YaMang/YaMangClient/YaMangClient/YaMangClient/ScenePlay.cpp
+++ YaMang/YaMangClient/YaMangClient/YaMangClient/ScenePlay.cpp
@@ -283,7 +283,7 @@ void ScenePlay::UpdateBase( int BaseIndex, int playerID )
 
 void ScenePlay::InitUIObjects()
 {
-	m_Minimap = new MiniMap(SPRITE_UI_MAP, SCENE_PLAY, 50, 0, false);
 	m_RegenBar = new UIObject(SPRITE_UI_REGEN_BAR, SCENE_PLAY, 100, 0, true);
 	m_RegenFlag = new UIObject(SPRITE_UI_REGEN_FLAG, SCENE_PLAY, 180, 30, true);
+	m_Minimap = new MiniMap(SPRITE_UI_MAP, SCENE_PLAY, 350, 100, false);
 }
