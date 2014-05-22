87be32fc3d974bce14c1458a30feac305c605ae7
1-ChungMoonChul <android_apps@limecolor.com>
2014-05-22 14:01:34 +0900

    병사 3명씩 갖고 게임 시작 [데모코드]

--- YaMang/YaMangServer/YaMangServer/GameRoom.cpp
+++ YaMang/YaMangServer/YaMangServer/GameRoom.cpp
@@ -86,6 +86,55 @@ void GameRoom::GameRoomStart()
 		const Corps* corps = GenerateCorps( client->GetPlayerID(), UnitType::UNIT_KING, kingPositionList.at( i ) );
 		m_KingIDList.push_back( corps->GetCorpsID() );
 		client->SetKingIndex( i, kingPositionList.at( i ).m_EyePoint.x, kingPositionList.at( i ).m_EyePoint.z );
+		
+
+
+
+		// test code
+		PositionInfo position;
+		if (kingPositionList.at(i).m_EyePoint.x > 0)
+		{
+			position.m_EyePoint = { kingPositionList.at(i).m_EyePoint.x - 30.0f, 0.0f, kingPositionList.at(i).m_EyePoint.z - 30.0f };
+			position.m_LookAtPoint = kingPositionList.at(i).m_LookAtPoint;
+		}
+		else
+		{
+			position.m_EyePoint = { kingPositionList.at(i).m_EyePoint.x + 30.0f, 0.0f, kingPositionList.at(i).m_EyePoint.z + 30.0f };
+			position.m_LookAtPoint = kingPositionList.at(i).m_LookAtPoint;
+		}
+		GenerateCorps(client->GetPlayerID(), UnitType::UNIT_ARROW, position);
+
+
+		PositionInfo position1;
+		if (kingPositionList.at(i).m_EyePoint.x > 0)
+		{
+			position1.m_EyePoint = { kingPositionList.at(i).m_EyePoint.x - 30.0f, 0.0f, kingPositionList.at(i).m_EyePoint.z };
+			position1.m_LookAtPoint = kingPositionList.at(i).m_LookAtPoint;
+		}
+		else
+		{
+			position1.m_EyePoint = { kingPositionList.at(i).m_EyePoint.x + 30.0f, 0.0f, kingPositionList.at(i).m_EyePoint.z };
+			position1.m_LookAtPoint = kingPositionList.at(i).m_LookAtPoint;
+		}
+		GenerateCorps(client->GetPlayerID(), UnitType::UNIT_KNIGHT, position1);
+
+		PositionInfo position2;
+		if (kingPositionList.at(i).m_EyePoint.x > 0)
+		{
+			position2.m_EyePoint = { kingPositionList.at(i).m_EyePoint.x, 0.0f, kingPositionList.at(i).m_EyePoint.z - 30.0f };
+			position2.m_LookAtPoint = kingPositionList.at(i).m_LookAtPoint;
+		}
+		else
+		{
+			position2.m_EyePoint = { kingPositionList.at(i).m_EyePoint.x, 0.0f, kingPositionList.at(i).m_EyePoint.z + 30.0f };
+			position2.m_LookAtPoint = kingPositionList.at(i).m_LookAtPoint;
+		}
+		GenerateCorps(client->GetPlayerID(), UnitType::UNIT_KNIGHT, position2);
+
+
+
+
+
 		++i;
 	}
 
